9b205e1ab959dd1df4b3d085ac175083c0cefedf
Cristian Valdés <cristian.valdes.v@gmail.com>
2025-04-05 19:08:27 -0300

    FIX app.js & Add server.js

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 const express = require('express');
-const app = express();
 const fs = require('fs');
-const users = JSON.parse(fs.readFileSync('./db.json')).users;
 
+const app = express();
 app.use(express.json());
+
+const users = JSON.parse(fs.readFileSync('./db.json')).users;
 
 app.get('/users', (req, res) => {
   res.status(200).json(users);
 });
-// http://localhost:3000/users/1
+
 app.get('/users/:id', (req, res) => {
   const user = users.find(u => u.id === parseInt(req.params.id));
   if (!user) return res.status(404).send('Usuario no encontrado');
   res.status(200).json(user);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Servidor corriendo en puerto ${PORT}`));
-
-module.exports = app; // For testing
+module.exports = app;
